bd1a6d38aab9e869916deb58c20a3055305c98f2
zirunyu521-arch <zirunyu521@gmail.com>
2025-12-17 23:08:34 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
+import Home from "./pages/Home";
 import { useState } from "react";
 import { AuthContext } from '@/contexts/authContext';
 
